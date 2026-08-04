c70c2b2128a3944975cf8a989a187f3a9f441f02
sihaowu1 <sihaowu1102@gmail.com>
2026-08-04 19:05:47 -0400

    polymarket data fetching

---
+++ fetch_data/polymarket.py
@@ -0,0 +1,62 @@
+import json
+import os
+import requests
+
+# =========================
+# CHANGE THE DATE HERE
+# =========================
+YEAR = 2026
+MONTH = 7
+DAY = 31
+# =========================
+
+
+EVENT = f"wti-closes-above-on-july-31-2026"
+
+GAMMA_API = "https://gamma-api.polymarket.com/events/slug/"
+CLOB_API = "https://clob.polymarket.com/prices-history"
+
+DATA_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
+def main() -> None:
+    """
+    1. get event
+    2. get all strikes
+    3. get minute interval trades for all strikes
+    """
+
+    # Get event
+    event = requests.get(
+        GAMMA_API + EVENT,
+        timeout=10
+    ).json()
+
+    data = {}
+
+    for market in event["markets"]:
+        # Get contract strike
+        strike = market["groupItemTitle"]
+
+        yes_token = json.loads(market["clobTokenIds"])[0]
+
+        # Get market history
+        history = requests.get(
+            CLOB_API,
+            params={
+                "market": yes_token,
+                "interval": "max",
+                "fidelity": 60,
+            },
+            timeout=10,
+        ).json()["history"]
+
+        data[strike] = history
+
+    out_path = os.path.join(DATA_DIR, f"{EVENT}.json")
+    with open(out_path, "w") as f:
+        json.dump(data, f)
+
+
+if __name__ == "__main__":
+    main()
